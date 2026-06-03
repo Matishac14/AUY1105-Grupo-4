8c33af71fddbce434507013d2eee00dee6fe52e5
Marcelo del Rio <mdelrio9606@gmail.com>
2026-06-02 23:40:21 -0400

    fix: fijar módulos vpc/ec2/s3 en tags publicados (v0.1.0/v0.2.0/v0.3.0)

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git::https://github.com/AUY1105-II/Modulos-AUY1105-Grupo-4.git//modules/vpc?ref=main"
+  source = "git::https://github.com/AUY1105-II/Modulos-AUY1105-Grupo-4.git//modules/vpc?ref=v0.1.0"
 
   project_name     = var.project_name
   environment      = var.environment
@@ -7,7 +7,7 @@ module "vpc" {
 }
 
 module "ec2" {
-  source = "git::https://github.com/AUY1105-II/Modulos-AUY1105-Grupo-4.git//modules/ec2?ref=main"
+  source = "git::https://github.com/AUY1105-II/Modulos-AUY1105-Grupo-4.git//modules/ec2?ref=v0.2.0"
 
   project_name       = var.project_name
   environment        = var.environment
@@ -16,7 +16,7 @@ module "ec2" {
 }
 
 module "s3" {
-  source = "git::https://github.com/AUY1105-II/Modulos-AUY1105-Grupo-4.git//modules/s3?ref=main"
+  source = "git::https://github.com/AUY1105-II/Modulos-AUY1105-Grupo-4.git//modules/s3?ref=v0.3.0"
 
   bucket_name = var.bucket_name
   environment = var.environment
